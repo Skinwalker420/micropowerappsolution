76fb4d4f9f5538c49df0694540113fbeed382cb2
Skinwalker420 <112089010+Skinwalker420@users.noreply.github.com>
2026-02-13 16:50:21 +0100

    tictactoe

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,14 +17,15 @@ namespace micropowerappsolution;
 /// </summary>
 public partial class MainWindow : Window
 {
+    bool player1Turn = false;
+    string[,] grid = { {"", "", ""}, { "", "", "" }, { "", "", "" } };
     public MainWindow()
     {
         InitializeComponent();
     }
-
-    void button_Click(object sender, RoutedEventArgs e)
+    void button_Click0(object sender, RoutedEventArgs e)
     {
-        StringBuilder condition = new StringBuilder(PCANBasic.MAX_LENGTH_HARDWARE_NAME);
+        /*StringBuilder condition = new StringBuilder(PCANBasic.MAX_LENGTH_HARDWARE_NAME);
         uint activate;
         MessageBoxResult result;
         TPCANStatus value = PCANBasic.GetValue(PCANBasic.PCAN_USBBUS1, TPCANParameter.PCAN_HARDWARE_NAME, condition, PCANBasic.MAX_LENGTH_HARDWARE_NAME);
@@ -37,6 +38,69 @@ public partial class MainWindow : Window
         {
             activate = PCANBasic.PCAN_PARAMETER_OFF;
         }
-        PCANBasic.SetValue(PCANBasic.PCAN_USBBUS1, TPCANParameter.PCAN_CHANNEL_IDENTIFYING, ref activate, sizeof(uint));
+        PCANBasic.SetValue(PCANBasic.PCAN_USBBUS1, TPCANParameter.PCAN_CHANNEL_IDENTIFYING, ref activate, sizeof(uint)); */
+        Vector pos = new Vector(0, 0);
+        Game_Input(button, pos);
+
     }
+    void button_Click1(object sender, RoutedEventArgs e)
+    {
+        Vector pos = new Vector(0, 1);
+        Game_Input(button1, pos);
+    }
+    void button_Click2(object sender, RoutedEventArgs e)
+    {
+        Vector pos = new Vector(0, 2);
+        Game_Input(button2, pos);
+    }
+    void button_Click3(object sender, RoutedEventArgs e)
+    {
+        Vector pos = new Vector(1, 0);
+        Game_Input(button3, pos);
+    }
+    void button_Click4(object sender, RoutedEventArgs e)
+    {
+        Vector pos = new Vector(1, 1);
+        Game_Input(button4, pos);
+    }
+    void button_Click5(object sender, RoutedEventArgs e)
+    {
+        Vector pos = new Vector(1, 2);
+        Game_Input(button5, pos);
+    }
+    void button_Click6(object sender, RoutedEventArgs e)
+    {
+        Vector pos = new Vector(2, 0);
+        Game_Input(button6, pos);
+    }
+    void button_Click7(object sender, RoutedEventArgs e)
+    {
+        Vector pos = new Vector(2, 1);
+        Game_Input(button7, pos);
+    }
+    void button_Click8(object sender, RoutedEventArgs e)
+    {
+        Vector pos = new Vector(2, 2);
+        Game_Input(button8, pos);
+    }
+
+    void Game_Input(Button inputbutton, Vector position)
+    {
+        if(inputbutton.Content != null) { return; }
+        player1Turn = !player1Turn;
+        if (player1Turn) { 
+            inputbutton.Content = "X";
+            grid[position.x, position.y] = "X";
+
+            return; 
+        }
+        inputbutton.Content = "O";
+        grid[position.x, position.y] = "O";
+    }
+}
+public class Vector
+{
+    public int x, y;
+
+    public Vector(int X, int Y) { x = X; y = Y; }
 }
